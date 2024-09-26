dfa19c39021ee2214dab753656004c5d8d6f4898
DevOL078 <oleinik.dima098@mail.ru>
2024-09-27 00:39:36 +0300

    Fix service name

--- src/main/kotlin/ru/oleynik/otus/maven/service/template/BillingServiceApp.kt
+++ src/main/kotlin/ru/oleynik/otus/maven/service/template/BillingServiceApp.kt
@@ -4,8 +4,8 @@ import org.springframework.boot.autoconfigure.SpringBootApplication
 import org.springframework.boot.runApplication
 
 @SpringBootApplication
-class MavenServiceTemplateApplication
+class BillingServiceApp
 
 fun main(args: Array<String>) {
-	runApplication<MavenServiceTemplateApplication>(*args)
+	runApplication<BillingServiceApp>(*args)
 }
